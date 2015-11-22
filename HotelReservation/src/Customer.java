cdd9b46c58dfcc8eb72baef9351b6c9e4094afcd
Ethan Huey <ethanhuey@gmail.com>
2015-11-22 17:21:08 -0500

    Revert "added customer and reservation instance variables"
    This reverts commit 6b94973d9835f76f94a223ee4fe39b76743816df.

--- HotelReservation/src/Customer.java
+++ HotelReservation/src/Customer.java
@@ -1,11 +1,4 @@
 
 public class Customer {
 
-	public String name;
-	public int customerID;
-	public String address;
-	public String ccType;//cc = credit card ...
-	public String ccNumber;
-	public String ccExpiration;
-
 }
--- HotelReservation/src/Reservation.java
+++ HotelReservation/src/Reservation.java
@@ -1,14 +1,4 @@
 
 public class Reservation {
 
-	public int customerID;
-	public int reservationID;
-	public int status;
-	public int startDate;
-	public int endDate;
-	public int roomType;
-	public boolean guaranteed;
-	public int numOccupants;
-	public int roomNumber;
-
 }
