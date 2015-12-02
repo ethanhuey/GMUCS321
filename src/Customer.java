110764745d25174d5724a5fc5cb3a07d8c1a7a93
saweel ahmed <saweelahmed@gmail.com>
2015-12-01 19:17:08 -0500

    added frameword classes

---
+++ src/Customer.java
@@ -0,0 +1,17 @@
+
+public class Customer {
+
+	public static String name;
+	public int customerID;
+	public String[] address;
+	public String ccType;
+	public String ccExpiration;
+	public String ccNumber;
+	
+	public Customer()
+	{
+		
+	}
+
+	
+}
