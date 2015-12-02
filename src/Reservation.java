a4043dd22c7fac7d87d427204181e49fbb51624f
saweel ahmed <saweelahmed@gmail.com>
2015-12-01 20:16:47 -0500

    update reservation instance variables

--- src/Reservation.java
+++ src/Reservation.java
@@ -3,12 +3,12 @@ public class Reservation {
 
 	public static int customerID;
 	public int reservationID;
-	public Object status;
-	public Object startDate;
-	public Object endDate;
-	public Object roomType;
-	public Object guaranteed;
-	public Object numOccupants;
-	public Object roomNumber;
+	public int status;
+	public int startDate;
+	public int endDate;
+	public int roomType;
+	public boolean guaranteed;
+	public int numOccupants;
+	public int roomNumber;
 
 }
